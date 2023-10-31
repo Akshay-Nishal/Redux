10c5107f473ff84d9a37ceba2c9c340ec209391a
Akshay <121626373+Akshay-Nishal@users.noreply.github.com>
2023-10-31 14:27:09 +0530

    Lets Optimize and Do it the right way

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,27 +1,39 @@
 import classes from './Counter.module.css';
 import {useSelector, useDispatch} from 'react-redux'
-
+import { counterActions } from '../store';
 const Counter = () => {
   const counter = useSelector(state=>state.counter)   //it automatically subscribe to redux store and always provide latest value/ If we unmount this component, it unsubscribe automatically
+  const showCounter = useSelector(state=>state.showCounter)   //it automatically subscribe to redux store and always provide latest value/ If we unmount this component, it unsubscribe automatically
   const dispatch = useDispatch()
-  const toggleCounterHandler = () => {};
-  const incrementHandler =()=>{
-    dispatch({type:'INCREMENT'})
-  }
-  const decrementHandler =()=>{
-    dispatch({type:'DECREMENT'})
 
+  const incrementHandler =()=>{
+    dispatch(counterActions.increment())
+  }
+
+  const decrementHandler =()=>{
+    dispatch(counterActions.decrement())
+  }
+
+  const toggleCounterHandler =()=>{
+    dispatch(counterActions.toggle())
+  }
+
+  const increaseHandler =()=>{
+    dispatch(counterActions.increase({amount:10}))     // if we dont use identifeer, its stored in action.payload. but when we add, its in action.payload.value
   }
 
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
+      {showCounter && <>
       <div className={classes.value}>-- {counter} --</div>
       <div>
-        <button onClick={incrementHandler} >Increment by 5</button>
-        <button disabled={counter<1} onClick={decrementHandler} >Decrement by 5</button>
+        <button onClick={incrementHandler} >Increment</button>
+        <button onClick={increaseHandler} >Increment by 10</button>
+        <button disabled={counter<1} onClick={decrementHandler} >Decrement</button>
       </div>
+      </>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,64 @@
 import {createStore} from 'redux'
+import {createSlice, configureStore} from '@reduxjs/toolkit'
 
-const counterReducer = (state={counter:0},action)=>{
-    if(action.type==='INCREMENT'){
-        return{
-            counter:state.counter+5
-        }
-    }
-    if(action.type==='DECREMENT'){
-        return{
-            counter:state.counter-5
-        }
-    }
-    return{
-        counter:state.counter
-
-    }
+const initialState={
+    counter:0,
+    showCounter:true
 }
 
-const store = createStore(counterReducer)
+const counterSlice = createSlice({
+    name:'counter',
+    initialState,
+    reducers:{
+        increment(state){
+            state.counter++;
+        },
+        increase(state,action){
+            console.log(action)
+            state.counter=state.counter+action.payload.amount
+        },
+        decrement(state){
+            state.counter--;
+        },
+        toggle(state){
+            state.showCounter=!state.showCounter
+        },
+    }
+})
+
+// const counterReducer = (state=initialState,action)=>{
+//     if(action.type==='INCREMENT'){
+//         return{
+//             counter:state.counter+1,
+//             showCounter:true
+//         }
+//     }
+//     if(action.type==='DECREMENT'){
+//         return{
+//             counter:state.counter-1,
+//             showCounter:true
+//         }
+//     }
+//     if(action.type==='TOGGLE'){
+//         console.log("Here")
+//         return{
+//             counter:state.counter,
+//             showCounter:!state.showCounter
+//         }
+//     }
+//     return{
+//         counter:state.counter,
+//         showCounter:true
+
+//     }
+// }
+
+
+const store = configureStore({      //it  helps to merge multople slice reducer 
+    reducer:counterSlice.reducer
+    // reducer:{counter:counterSlice.reducer,}    by this we can merge multiple reducer
+})
+
+export const counterActions = counterSlice.actions    //it autogenerates actions for us
 
 export default store
